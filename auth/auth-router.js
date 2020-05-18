cdc38ea22646a6dfc859a2fce39ec7f77a1bb70b
Luis Hernandez <luishrd@gmail.com>
2020-05-18 16:16:37 -0600

    adds roles table to migrations

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,18 +18,17 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-    .first()
-    .then(user => {
+    .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {

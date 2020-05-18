129a6aa96785166329ddd71733096da994a7e51c
Luis Hernandez <luishrd@gmail.com>
2020-05-18 16:31:14 -0600

    adds status code to get users

--- users/users-router.js
+++ users/users-router.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, (req, res) => {
+router.get("/", restricted, (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.status(200).json(users);
     })
     .catch(err => res.send(err));
 });

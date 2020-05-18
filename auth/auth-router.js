d97198ac3dd18a1d9ef35d2d2fd52285b6f147e3
Luis Hernandez <luishrd@gmail.com>
2020-05-18 16:46:03 -0600

    adds user validation

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,39 +1,57 @@
+const bcryptjs = require("bcryptjs");
+
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
 
 const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
 
-// for endpoints beginning with /api/auth
 router.post("/register", (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+  const credentials = req.body;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
 
 router.post("/login", (req, res) => {
-  let { username, password } = req.body;
+  const { username, password } = req.body;
 
-  Users.findBy({ username })
-    .then(([user]) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          res.status(200).json({ message: "Welcome to our API" });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
 
 module.exports = router;

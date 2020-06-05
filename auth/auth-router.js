a5bf1dadcbaf62cb5d62f86bffb99bf5d1f59a67
Veto Ramirez <vetorobert3@gmail.com>
2020-06-04 19:10:33 -0700

    added a bunch of stuff

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
-
 const router = require("express").Router();
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 const Users = require("../users/users-model.js");
 const { isValid } = require("../users/users-service.js");
@@ -39,7 +40,11 @@ router.post("/login", (req, res) => {
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          const token = generateToken(user);
+          res.status(200).json({ 
+            message: "Welcome to our API" ,
+            token
+          });
         } else {
           res.status(401).json({ message: "Invalid credentials" });
         }
@@ -53,5 +58,19 @@ router.post("/login", (req, res) => {
     });
   }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role
+  };
+
+  const options = {
+    expiresIn: '2h'
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;

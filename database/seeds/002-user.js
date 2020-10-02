5a5f80e31198252b29aaf9b2fcaf85362239389e
ebisLab <ebaiden@techie.com>
2020-10-02 11:18:58 -0400

    added users and clients seeds

--- database/seeds/002-user.js
+++ database/seeds/002-user.js
@@ -15,7 +15,89 @@ exports.seed = function(knex) {
           bio: "I am THE Johnny Bravo. Wanna see me comb my hair, really fast? Hit me up and I'll show you",
           profession: "Barber",
           location: "Metropolis"
-        }
+        },
+        {
+          id: 1,
+          username: "pcbh",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/PXeqdsH.jpg",
+          first: "Princess Consuela",
+          last: "Banana-Hammock",
+          profession: "Cosmetologist",
+          location: "Washington",
+          bio:
+            "Hello, If you are interested in any of my services please consult with me if you are a new customer.",
+        },
+        {
+          id: 2,
+          username: "mscof",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/ILgo4FN.jpg",
+          first: "Michael",
+          last: "Scofield",
+          profession: "Barber",
+          location: "Chicago",
+          bio:
+            "I specialize in the sickest fades out here in Chi town. Check me out",
+        },
+        {
+          id: 3,
+          username: "adamgirl",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/DjNO3KI.jpg",
+          first: "Wednesday",
+          last: "Addams",
+          profession: "Nail Tech",
+          location: "Montana",
+          bio: "Nails everyone? Come through, I'll pamper you like crazy",
+        },
+        {
+          id: 4,
+          username: "cmerry",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/gHcpx9R.jpg",
+          first: "Cindy",
+          last: "Merrywether",
+          profession: "Colorist",
+          location: "Metropolis",
+          bio:
+            "My duty is to enhance your outer beauty and make you look even more fabulous",
+        },
+        {
+          id: 5,
+          username: "dragonTat",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/FI3NTFC.jpg",
+          first: "Lisbeth",
+          last: "Salander",
+          profession: "Makeup Artist",
+          location: "LA",
+          bio: "Makeup can help you capture a moment. ~ Carine Roitfeld",
+        },
+        {
+          id: 6,
+          username: "bbarnes",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/gCFZiyD.jpg",
+          first: "Bucky",
+          last: "Barnes",
+          profession: "Loctician",
+          location: "Metropolis",
+          bio: "Quality service for an affordable price ",
+        },
+        {
+          id: 7,
+          username: "lcage",
+          password: "2020mess",
+          imgUrl: "https://i.imgur.com/sjpQ78p.jpg",
+          first: "Luke",
+          last: "Cage",
+          profession: "Salon/Spa Owner",
+          location: "Orlando",
+          bio:
+            "I specialize in enhancing your health to upgrade your beauty. ",
+        },
+      
       ]);
     });
 };
--- database/seeds/003-clients.js
+++ database/seeds/003-clients.js
@@ -24,8 +24,156 @@ exports.seed = function(knex) {
           "user_id": 0,
           "client_name": "Wanda",
           "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/PmVHgFS.jpg"
+        },
+        {
+          "id": 3,
+          "user_id": 1,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/cX7ccxK.jpg"
+        },
+        {
+          "id": 4,
+          "user_id": 1,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/CQaZyta.jpg"
+        },
+        {
+          "id": 5,
+          "user_id": 1,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/Nqq8Te4.jpg"
+        },
+        {
+          "id": 6,
+          "user_id": 2,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/ILgo4FN.jpg"
+        },
+        {
+          "id": 7,
+          "user_id": 2,
+          "client_name": "Vision",
+          "service": "blow out",
           "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
         },
+        {
+          "id": 8,
+          "user_id": 2,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/RPDgUMm.jpg"
+        },
+        {
+          "id": 9,
+          "user_id": 3,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/ILgo4FN.jpg"
+        },
+        {
+          "id": 10,
+          "user_id": 3,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/CQaZyta.jpg"
+        },
+        {
+          "id": 11,
+          "user_id": 3,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
+        },
+        {
+          "id": 12,
+          "user_id": 4,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/MRDpqHj.jpg"
+        },
+        {
+          "id": 13,
+          "user_id": 4,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
+        },
+        {
+          "id": 14,
+          "user_id": 4,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/RPDgUMm.jpg"
+        },
+        {
+          "id": 15,
+          "user_id": 5,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/ILgo4FN.jpg"
+        },
+        {
+          "id": 16,
+          "user_id": 5,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
+        },
+        {
+          "id": 17,
+          "user_id": 5,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/RPDgUMm.jpg"
+        },
+        {
+          "id": 18,
+          "user_id": 6,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
+        },
+        {
+          "id": 19,
+          "user_id": 6,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/MRDpqHj.jpg"
+        },
+        {
+          "id": 20,
+          "user_id": 6,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/hZdkwdC.jpg"
+        },
+        {
+          "id": 21,
+          "user_id": 7,
+          "client_name": "Jarvis",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/0egAGWj.jpg"
+        },
+        {
+          "id": 22,
+          "user_id": 7,
+          "client_name": "Vision",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/CQaZyta.jpg"
+        },
+        {
+          "id": 23,
+          "user_id": 7,
+          "client_name": "Wanda",
+          "service": "blow out",
+          "client_ImgUrl": "https://i.imgur.com/PmVHgFS.jpg"
+        },
+        
       ]);
     });
 };

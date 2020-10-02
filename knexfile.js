b29418ecfb366c234cbc4b9bfde2bed338df8624
ebisLab <ebaiden@techie.com>
2020-10-02 09:39:11 -0400

    pg support

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+const pgConnection = process.env.DATABASE_URL || "postgres://ywpfkpsxplftnm:c0fc239cb0b2ba329d1d108d977f49e3c776ea9835c494381f4369e97af42adb@ec2-18-211-86-133.compute-1.amazonaws.com:5432/d6h97b06f9brfu";
 // if using a local postgres server, please create the database manually, Knex will not create it autmatically
 
 module.exports = {

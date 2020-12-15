6f5ce070d062e774bcb7fd677366a7ae63998e3e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-15 15:34:00 +0100

    get ready

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const authRouter = require("../auth/auth-router.js");
-const usersRouter = require("../users/users-router.js");
+const authRouter = require("./auth/auth-router.js");
+const usersRouter = require("./users/users-router.js");
 
 const server = express();
 

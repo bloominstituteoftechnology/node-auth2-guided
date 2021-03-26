e9b6e66f0bedcbb294484963c3597f4adab484bb
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-26 17:40:54 +0100

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, (req, res) => {
+router.get("/", restricted, (req, res, next) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
-    .catch(err => res.send(err));
+    .catch(next); // our custom err handling middleware in server.js will trap this
 });
 
 module.exports = router;

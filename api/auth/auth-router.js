a5565dc1f02e614ea13c13980aab2f549589af36
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-26 15:57:19 +0100

    updates

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,57 +1,41 @@
-const bcryptjs = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
-const Users = require("../users/users-model.js");
-const { isValid } = require("../users/users-service.js");
+const Users = require('../users/users-model.js');
+const checkCredentials = require('./check-payload-middleware');
 
-router.post("/register", (req, res) => {
-  const credentials = req.body;
+router.post('/register', checkCredentials, (req, res, next) => {
+  let user = req.body;
 
-  if (isValid(credentials)) {
-    const rounds = process.env.BCRYPT_ROUNDS || 8;
+  // bcrypting the password before saving
+  const rounds = process.env.BCRYPT_ROUNDS || 8; // 2 ^ 8
+  const hash = bcrypt.hashSync(user.password, rounds);
 
-    // hash the password
-    const hash = bcryptjs.hashSync(credentials.password, rounds);
+  // never save the plain text password in the db
+  user.password = hash
 
-    credentials.password = hash;
-
-    // save the user to the database
-    Users.add(credentials)
-      .then(user => {
-        res.status(201).json({ data: user });
-      })
-      .catch(error => {
-        res.status(500).json({ message: error.message });
-      });
-  } else {
-    res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
-    });
-  }
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(next); // our custom err handling middleware in server.js will trap this
 });
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+router.post('/login', checkCredentials, (req, res, next) => {
+  let { username, password } = req.body;
 
-  if (isValid(req.body)) {
-    Users.findBy({ username: username })
-      .then(([user]) => {
-        // compare the password the hash stored in the database
-        if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
-        } else {
-          res.status(401).json({ message: "Invalid credentials" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: error.message });
-      });
-  } else {
-    res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
-    });
-  }
+  Users.findBy({ username }) // it would be nice to have middleware do this
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -18,4 +18,11 @@ server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
 module.exports = server;

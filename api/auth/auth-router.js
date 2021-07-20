1b54df60d51035e337babb8608053931280c383d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-20 16:30:38 +0200

    rename variable

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
-const checkCredentials = require('./check-payload-middleware');
+const checkAuthPayload = require('./check-payload-middleware');
 
-router.post('/register', checkCredentials, (req, res, next) => {
+router.post('/register', checkAuthPayload, (req, res, next) => {
   let user = req.body;
 
   // bcrypting the password before saving
@@ -24,7 +24,7 @@ router.post('/register', checkCredentials, (req, res, next) => {
     .catch(next); // our custom err handling middleware in server.js will trap this
 });
 
-router.post('/login', checkCredentials, (req, res, next) => {
+router.post('/login', checkAuthPayload, (req, res, next) => {
   let { username, password } = req.body;
 
   Users.findBy({ username }) // it would be nice to have middleware do this

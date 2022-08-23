ebbb61e60b84934a85e97df181e9318b256d7121
James Blacklock <jamesblacklock@protonmail.com>
2022-08-23 12:29:04 -0400

    committing to "lecture"

--- config/index.js
+++ config/index.js
@@ -2,4 +2,5 @@ module.exports = {
   BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 9000,
+  JWT_SECRET: 'my super secret password',
 }

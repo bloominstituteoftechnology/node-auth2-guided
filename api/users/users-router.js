38dbf6870f18b1f326ff49483fad0ffdcfd37cf6
James Blacklock <jamesblacklock@protonmail.com>
2022-08-23 12:50:05 -0400

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,6 +5,9 @@ const User = require("./users-model.js")
 const { restricted } = require('../auth/auth-middleware')
 
 router.get("/", restricted, (req, res, next) => {
+
+  console.log(req.headers);
+
   User.find()
     .then(users => {
       res.json(users)
